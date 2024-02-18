7126bfab9deb3c2138c7b6ed2e5a82816ff5c8b8
John Heger <heger7@gmail.com>
2024-02-18 12:00:57 -0600

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr.Chocolate", 2, "M", "brown", "easy-going", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 4, "M", "white", "vicious", 1);
+INSERT INTO bears VALUES(3, "Tabitha", 3, "F", "black", "chill", 0);
+INSERT INTO bears VALUES(4, "Sergeant Brown", 7, "M", "brown", "uptight", 0);
+INSERT INTO bears VALUES(5, "Melissa", 15, "F", "orange", "relaxed", 1);
+INSERT INTO bears VALUES(6, "Grinch", 12,"M", "pink", "happy", 1);
+INSERT INTO bears VALUES(7, "Wendy", 20, "F", "turquoise", "angry", 0);
+INSERT INTO bears VALUES(8, null, 35, "M", "red", "calm", 1);
